5fdcdb8f8ace549966c6da638084bac54c2c2a5e
felix <felix.825210093@stu.untar.ac.id>
2023-09-28 21:00:00 +0700

    exit commit

--- lib/about.dart
+++ lib/about.dart
@@ -4,9 +4,7 @@ class AboutScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('About'),
-      ),
+
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
